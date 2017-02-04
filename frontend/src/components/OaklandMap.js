9f93a45f33e936fbf1667be35df592db765c2758
Monica Williams <monicaestellaw@gmail.com>
2017-02-04 11:33:39 -0800

    we now have oakland crime data

--- frontend/src/components/OaklandMap.js
+++ frontend/src/components/OaklandMap.js
@@ -3,6 +3,45 @@ import { Map, Marker, InfoWindow } from 'google-maps-react'
 
 
 export default class OaklandMap extends Component {
+
+  constructor( props ) {
+    super( props )
+    this.state = {
+      OaklandSchoolsData: [],
+      OaklandCrimeData: []
+    }
+  }
+
+  componentDidMount() {
+    this.getOaklandSchoolsData()
+    this.getOaklandCrimeData()
+  }
+
+  getOaklandSchoolsData() {
+    fetch( 'https://data.oaklandnet.com/resource/d6pc-iyaw.json', {
+      method: 'GET',
+      mode: 'cors'
+    })
+    .then( response => response.json() )
+    .then( results => {
+      this.setState({
+        OaklandSchoolsData: results
+      })
+    })
+  }
+  getOaklandCrimeData() {
+    fetch( 'http://oakland.crimespotting.org/crime-data?format=json&dstart=2013-01-01&dend=2013-12-31&count=150', {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then( response => response.json() )
+    .then( results => {
+      this.setState({
+        OaklandCrimeData: results
+      })
+    })
+  }
+
   render () {
     return (
   <Map google={window.google} zoom={10} initialCenter={{lat: 37.8044, lng: -122.2711}}>
