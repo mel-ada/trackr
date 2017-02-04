7d14f4fd4ddc5ce2cc0c0e2b0100401be13592bc
Monica Williams <monicaestellaw@gmail.com>
2017-02-03 23:22:55 -0800

    got map to render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
+import Map from 'google-maps-react'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Map google={window.google} zoom={14}>
+
+          {/* <Marker onClick={this.onMarkerClick}
+          name={'Current location'} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow> */}
+        </Map>
       </div>
     )
   }
